fc231f58de44419bcc4ec3bee3bf73a24596415c
lpy817 <167194851+lpy817@users.noreply.github.com>
2024-05-25 19:04:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,14 +19,14 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '九万里风鹏正举，举目起壮志,一百年长河浩荡，荡胸生豪情,与其凋零，不如尽情燃烧',
+    '九万里风鹏正举，举目起壮志,一百年长河浩荡，荡胸生豪情,与其凋零，不如尽情燃烧,追求的后面没有句号，人生也永远没有太晚的开始,于高山之巅，方见大河奔涌；于群峰之上，更觉长风浩荡,我居北海君南海，寄雁传书谢不能,桃李春风一杯酒，江湖夜雨十年灯',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的早八人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '小李同学的学习记录', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
